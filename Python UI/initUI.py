6b99dbbfd700e6e217e2709e7831295ee5812e17
Stelios Vazaios <49122833+Vazeagle@users.noreply.github.com>
2020-04-07 22:40:57 +0300

    commit

---
+++ Python UI/initUI.py
@@ -0,0 +1,11 @@
+import os 
+from tkinter import *
+from tkinter import messagebox
+from tkinter import ttk
+from PIL import Image, ImageTk
+import pyautogui
+from datetime import datetime
+
+mydir=os.getcwd()
+print(mydir)
+print("HELLO WORLD")
