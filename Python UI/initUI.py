c655afcd0b5ccb38eec0fbb781970a7eae9e7f91
Stelios Vazaios <49122833+Vazeagle@users.noreply.github.com>
2020-04-09 03:52:51 +0300

    commit

--- Python UI/initUI.py
+++ Python UI/initUI.py
@@ -21,10 +21,10 @@ def main():
     main_window =Tk()
     #main_window.geometry(resolution) ###########################################resolution
     main_window.title("Parmenidis")
-    main_window.configure(background='salmon1')
+    main_window.configure(background='green')
     # ---------------------------------------------------
     container = ttk.Frame(main_window ,width=getRes[0], height=getRes[1])
-    canvas = Canvas(container, bg="salmon1" ,width=getRes[0 ] -50, height=getRes[1 ] -70 ,borderwidth=0
+    canvas = Canvas(container, bg="blue" ,width=getRes[0 ] -50, height=getRes[1 ] -70 ,borderwidth=0
                     ,highlightthickness=0)
     scrollbarv = ttk.Scrollbar(container, orient="vertical", command=canvas.yview)
     scrollbarh = ttk.Scrollbar(container, orient="vertical", command=canvas.xview)
@@ -56,45 +56,47 @@ def main():
 
 
 
-    init_Label = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1")
+    init_Label = Label(intro_Frame ,borderwidth=1 ,highlightthickness=0, bg="salmon1")
 
 
     label_left = Label(init_Label ,text="Simplified-DES Encryption\Decryption\n", bg="salmon1",font=("Calibri", 24 ,"bold")  )  # aristero menu
-    label_l_up = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # photo parmenidi
-    label_l_down = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # button gia menu kai alla frames
+    label_l_up = Label(label_left ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # photo parmenidi
+    label_l_down = Label(label_left ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # button gia menu kai alla frames
 
-    label_right = Label(init_Label ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # dexio menu
-    label_r_up = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # panw meros me perilipsh kai hmerologio
-    label_ru_up = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # kalos orisate+ to eniaio susthma klp
-    label_ruu_up = Label(intro_Frame ,text="Καλώς ήρθατε στον Παρμενίδη!\n" ,borderwidth=0 ,highlightthickness=0, bg="salmon1" ,font=("Calibri", 24 ,"bold"))
-    label_ruu_down = Label(intro_Frame ,text="Το ενιαίο σύστημα για τις Πανελλήνιες Εξετάσεις.\n" ,borderwidth=0,highlightthickness=0, bg="salmon1" ,font=("Calibri", 18))
+    label_right = Label(init_Label ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # dexio menu
+    label_r_up = Label(label_right ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # panw meros me perilipsh kai hmerologio
+    label_ru_up = Label(label_r_up ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # kalos orisate+ to eniaio susthma klp
+    label_ruu_up = Label(label_ru_up ,text="Καλώς ήρθατε στον Παρμενίδη!\n" ,borderwidth=0 ,highlightthickness=0, bg="salmon1" ,font=("Calibri", 24 ,"bold"))
+    label_ruu_down = Label(label_ru_up ,text="Το ενιαίο σύστημα για τις Πανελλήνιες Εξετάσεις.\n" ,borderwidth=0,highlightthickness=0, bg="salmon1" ,font=("Calibri", 18))
 
-    label_ru_left = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # foto parmenidi+ desription parmenidi
-    label_rul_left = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # foto parmenidi
-    label_rul_right = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # Ο παρμενιδης ειναι bla bla
+    #isws na 8elei up kai oxi left
+    label_ru_left = Label(label_r_up ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # foto parmenidi+ desription parmenidi
+    label_rul_left = Label(label_ru_left ,text="PHOTO PARMENIDI", borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # foto parmenidi
+    label_rul_right = Label(label_ru_left ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # Ο παρμενιδης ειναι bla bla
 
-    label_ru_right = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # hmerologio gramma + hmerologio
-    label_rur_up = Label(intro_Frame ,text="Ημερολόγιο\n" ,borderwidth=0 ,highlightthickness=0, bg="salmon1"
-                         ,font=("Calibri", 18)  )  # hmerologio gramma
-    label_rur_down = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1")  # hmerologio
+    label_ru_right = Label(label_r_up ,borderwidth=0 ,highlightthickness=1, bg="salmon1"  )  # hmerologio gramma + hmerologio
+    label_rur_up = Label(label_ru_right ,text="Ημερολόγιο\n" ,borderwidth=1 ,highlightthickness=0, bg="salmon",font=("Calibri", 18)  )  # hmerologio gramma
+    label_rur_down = Label(label_ru_right ,borderwidth=1 ,highlightthickness=0, bg="salmon1")  # hmerologio
 
-    label_r_down = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # sunoptiko profil
-    label_rd_up = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # sunoptiko profil
-    label_rd_down = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # onoma xrhsth kai alla
-    label_rdd_left = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # eikona user
-    label_rdd_right = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # onoma xrhsth status lukeio kai alla
+    label_r_down = Label(label_right ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # sunoptiko profil
+    label_rd_up = Label(label_r_down ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # sunoptiko profil
+    label_rd_down = Label(label_r_down ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # onoma xrhsth kai alla
+    label_rdd_left = Label(label_rd_down ,borderwidth=1,highlightthickness=0, bg="salmon1"  )  # eikona user
+    label_rdd_right = Label(label_rd_down ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # onoma xrhsth status lukeio kai alla
 
-    label_rddr_left = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # onoma xrhsth kai status
-    label_rddrl_up = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # onoma xrhsth
-    label_rddrl_down = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # katastash
-    label_rddrld_left = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # katastash text
-    label_rddrld_right = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # input katastashs apo data base
+    label_rddr_left = Label(label_rdd_right ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # onoma xrhsth kai status
+    label_rddrl_up = Label(label_rddr_left ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # onoma xrhsth
+    label_rddrl_down = Label(label_rddr_left ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # katastash
+    label_rddrld_left = Label(label_rddrl_down ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # katastash text
+    label_rddrld_right = Label(label_rddrl_down ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # input katastashs apo data base
 
-    label_rddr_right = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # lukeio kai kateu8unsh
-    label_rddrr_top = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # lukeio
-    label_rddrr_down = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # katey8unsh
-    label_rddrrd_left = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # kateu8unsh text
-    label_rddrrd_right = Label(intro_Frame ,borderwidth=0 ,highlightthickness=0, bg="salmon1"  )  # kateu8unsh input
+    label_rddr_right = Label(label_rdd_right ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # lukeio kai kateu8unsh
+    label_rddrr_up = Label(label_rddr_right ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # lukeio genika
+    label_rddrru_left = Label(label_rddrr_up ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # lukeio gramma
+    label_rddrru_right = Label(label_rddrr_up ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # lukeio input from db
+    label_rddrr_down = Label(label_rddr_right ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # katey8unsh
+    label_rddrrd_left = Label(label_rddrr_down ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # kateu8unsh text
+    label_rddrrd_right = Label(label_rddrr_down ,borderwidth=1 ,highlightthickness=0, bg="salmon1"  )  # kateu8unsh input
 
     descriptionText =Text(label_rul_right ,height=7 ,bg="salmon1" ,borderwidth=0 ,highlightthickness=0,font=("Calibri", 14), width = 100)
     descriptionText.insert(INSERT,"Ο Παρμενίδης ήταν αρχαίος έλληνας φιλόσοφος. Γεννήθηκε στην Ελέα της Μεγάλης Ελλάδας\n στα τέλητου 6ου αιώνα π.Χ., ....")
@@ -123,6 +125,7 @@ def main():
     # P3 = Label(bottomLabel,image=render0,borderwidth=0,highlightthickness=0)
     # P3 = Label(bottomLabel,padx=(getRes[0]/10),bg="salmon1",borderwidth=0,highlightthickness=0)
     # P4 = Label(bottomLabel,image=render3,borderwidth=0,highlightthickness=0)
+    # P1.pack(side=LEFT)
 
     # P0=Label(bottomLabel,image=render0,borderwidth=0,highlightthickness=0)
     # P0.place(x=0, y=0, relwidth=1, relheight=1)
@@ -130,8 +133,50 @@ def main():
     # topTitle.pack(side=TOP)
     # topLabel2.pack(side=TOP)
     # descriptionText.pack()
+    #######################################     EMFANISH TOU UI apo ta teleutaia labels pros ta arxika
 
+    label_left.pack(side=LEFT)
+    label_l_up.pack(side=TOP)
+    label_l_down.pack(side=BOTTOM)
 
+    label_right.pack(side=RIGHT)
+    label_r_up.pack(side=TOP)
+    label_ru_up.pack(side=TOP)
+    label_ruu_up.pack(side=TOP)
+    label_ruu_down.pack(side=BOTTOM)
+
+    #isws na 8elei up kai oxi left
+    label_ru_left.pack(side=LEFT)
+    label_rul_left.pack(side=LEFT)
+    label_rul_right.pack(side=RIGHT)
+
+    label_ru_right.pack(side=RIGHT)
+    label_rur_up.pack(side=TOP)
+    label_rur_down.pack(side=BOTTOM)
+
+    label_r_down.pack(side=BOTTOM)
+    label_rd_up.pack(side=TOP)
+    label_rd_down.pack(side=BOTTOM)
+    label_rdd_left.pack(side=LEFT)
+    label_rdd_right.pack(side=RIGHT)
+
+    label_rddr_left.pack(side=LEFT)
+    label_rddrl_up.pack(side=TOP)
+    label_rddrl_down.pack(side=BOTTOM)
+    label_rddrld_left.pack(side=LEFT)
+    label_rddrld_right.pack(side=RIGHT)
+
+    label_rddr_right.pack(side=RIGHT)
+    label_rddrr_up.pack(side=TOP)
+    label_rddrru_left.pack(side=LEFT)
+    label_rddrru_right.pack(side=RIGHT)
+    label_rddrr_down.pack(side=BOTTOM)
+    label_rddrrd_left.pack(side=LEFT)
+    label_rddrrd_right.pack(side=RIGHT)
+
+    
+    ###########################################
+    
     butttonNext0.pack(side=TOP)
     butttonNext1.pack(side=TOP)
     butttonNext2.pack(side=TOP)
@@ -142,7 +187,7 @@ def main():
     butttonNext7.pack(side=TOP)
     butttonNext8.pack(side=TOP)
 
-    # P1.pack(side=LEFT)
+
 
 
     raise_frame(intro_Frame)
