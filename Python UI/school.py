2631a702ed7ced1d0323a50a6054300bb2692960
Stelios Vazaios <49122833+Vazeagle@users.noreply.github.com>
2020-05-15 19:07:03 +0300

    inital stage of page of school user

---
+++ Python UI/school.py
@@ -0,0 +1,122 @@
+from tkinter import *
+from tkinter import filedialog
+from tkinter import messagebox
+from tkinter import ttk
+from PIL import Image, ImageTk, ImageDraw, ImageFilter
+import pyautogui
+from datetime import datetime
+from tkcalendar import Calendar, DateEntry
+
+
+#Initialisation
+getRes = pyautogui.size()
+resolution = str(getRes[0]) + "x" + str(getRes[1])
+main_window = Tk()
+main_window.geometry(resolution) ###########################################resolution
+main_window.title("Parmenidis")
+main_window.configure()
+main_window.state("zoomed")
+none="none" # προσωρινο για μεταβαση σε frames
+previous_frame="previous_frame"
+frame_counter=0
+init_pass=0
+
+
+#Frames For Main Window
+frame_temp=Frame()#Frame to get as temp to successfull change between frames
+school_Frame=Frame(main_window, bg="white")
+school_menu_Frame=Frame(school_Frame, bg="gray26")
+school_intro_Frame = Frame(school_Frame, bg="floral white")
+school_Dates_Frame = Frame(school_Frame, bg="floral white")
+school_exams_Frame = Frame(school_Frame, bg="floral white")
+school_program_Frame = Frame(school_Frame, bg="floral white")
+school_student_reg = Frame(school_Frame, bg="floral white")
+
+def ExitApp():
+    MsgBox = messagebox.askquestion('Έξοδος Εφαρμογής!', 'Είστε σίγουροι ότι θέλετε να αποσυνδεθείτε από το σύστημα Παρμενίδης ;', icon='warning')
+    if MsgBox == 'yes':
+        main_window.destroy()
+    else:
+        messagebox.showinfo('Επιστροφή', 'Θα επιστραφείτε στην προηγούμενη σας οθόνη !')
+
+def raiseNdrop_frame(frameUp,frameDown):
+    global frame_counter
+    global init_pass #flag to see if menu frame has appeared (0 is  no, 1 is yes)
+    global frame_temp #απλα οριζω οτι το frame temp εινια τυπου frame γιατι αλλιως προβλημα στο forget γτ το διαβάζει ως string
+    #print(frameDown)
+
+    if(frameDown!="none"):
+        if(frameDown=="previous_frame"):#forget previous frame
+            frame_counter=0
+            #print("going to delete frame: ",frame_temp)
+            frame_temp.pack_forget()    
+        else:
+            frameDown.pack_forget()
+    
+    if(frame_counter==0 and init_pass==1 ):   #frame to close (memory)
+        temp2=0
+        frame_counter=1
+        frame_temp=frameUp
+        #print("memory",frame_temp)
+    
+ 
+
+    if(frameUp==school_menu_Frame):
+        frameUp.tkraise()
+        frameUp.pack(side=LEFT, fill=Y)
+        init_pass=1
+    else:
+        frameUp.tkraise()
+        frameUp.pack(expand=1,fill=BOTH)
+
+#LOAD PHOTO
+load2 = Image.open('P2.gif')
+load2 = load2.resize((100, 100), Image.ANTIALIAS)
+render2 = ImageTk.PhotoImage(load2)
+
+def main():
+
+    school_menu = Label(school_menu_Frame, bg="gray26",font=("Calibri", 24, "bold"))  # aristero menu
+    smenu_l_up = Label(school_menu, image=render2, borderwidth=1, highlightthickness=0, bg="gray26")  # photo parmenidi
+    smenu_l_down = Label(school_menu, borderwidth=1, highlightthickness=0,bg="gray26")  # button gia menu kai alla frames
+
+    initialSchool_all = Label(school_intro_Frame, borderwidth=1, highlightthickness=0, bg="red")  # dexia arxikh selida
+    #add labels for this initial page and then pack to reveal 
+
+    btn_next0 = Button(smenu_l_down, text="Αρχική Σελίδα", command=lambda: raiseNdrop_frame(school_intro_Frame,previous_frame), bg="gray26",height = 2, width = 35,font=("Calibri", 14, "bold"))
+    btn_next1 = Button(smenu_l_down, text="Πρόγραμματα", command=lambda: raiseNdrop_frame(statement_Frame,previous_frame), bg="gray26",height = 2, width = 35,font=("Calibri", 14, "bold"))
+    btn_next2 = Button(smenu_l_down, text="Εγγραφές", command=lambda: raiseNdrop_frame(all_programs_Frame,previous_frame), bg="gray26",height = 2, width = 35,font=("Calibri", 14, "bold")) 
+    btn_next3 = Button(smenu_l_down, text="Έξοδος", command=lambda: ExitApp(), bg="gray26",height = 2, width = 35,font=("Calibri", 14, "bold"))
+
+    #packs
+    initialSchool_all.pack(side=TOP,fill=BOTH,expand=1)
+    school_menu.pack(side=LEFT,expand=1,fill=Y)
+    smenu_l_up.pack(side=TOP,pady=50)#PARMENIDIS LOGO
+    smenu_l_down.pack(side=TOP)#CONTAINS BUTTONS
+    #buttons
+    btn_next0.pack(side=TOP,pady=2,ipady=5)
+    btn_next1.pack(side=TOP,pady=2,ipady=5)
+    btn_next2.pack(side=TOP,pady=2,ipady=5)
+    btn_next3.pack(side=TOP,pady=2,ipady=5)    
+
+
+    #---------------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------------
+
+
+
+    raiseNdrop_frame(school_Frame,none)
+    raiseNdrop_frame(school_menu_Frame,none)
+    raiseNdrop_frame(school_intro_Frame,none)  
+
+
+
+
+
+    main_window.mainloop()  # ------------------------------Put always to end of frames
+
+main()
