00786261265dd3f8e149e671c500f6d027a3dfe6
stergiop <fotinistergiopoulou@yahoo.com>
2020-05-22 17:55:50 +0300

    code

--- Python UI/board.py
+++ Python UI/board.py
@@ -29,12 +29,70 @@ load2 = Image.open('P2.gif')
 load2 = load2.resize((100, 100), Image.ANTIALIAS)
 render2 = ImageTk.PhotoImage(load2)
 
+###frames
 frame_temp=Frame()#Frame to get as temp to successfull change between frames
 all_Frame=Frame(main_window, bg="white")
 menu_Frame=Frame(all_Frame, bg="gray26")
 intro_Frame = Frame(all_Frame, bg="floral white")
+statement_Frame1=Frame(all_Frame, bg="floral white")
+Applications=Frame(all_Frame, bg="floral white")
+HistoryApps=Frame(all_Frame, bg="floral white")
+PendingApps=Frame(all_Frame, bg="floral white")
+States=Frame(all_Frame, bg="floral white")
+Apply=Frame(all_Frame, bg="floral white")
+FinalStates=Frame(all_Frame, bg="floral white")
+PanhellenicExams=Frame(all_Frame, bg="floral white")
+Program=Frame(all_Frame, bg="floral white")
+GradingCenterSubmission=Frame(all_Frame, bg="floral white")
+SupervisorsList=Frame(all_Frame, bg="floral white")
+AcceptDeny=Frame(all_Frame, bg="floral white")
+Processing=Frame(all_Frame, bg="floral white")
+HistoryList=Frame(all_Frame, bg="floral white")
+Shuffle=Frame(all_Frame, bg="floral white")
+Problems=Frame(all_Frame, bg="floral white")
+Exitt=Frame(all_Frame, bg="floral white")
+
+
+
+
 
 def main():
+label_left = Label(menu_Frame, text="\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n\n", bg="gray26",font=("Calibri", 24, "bold"))  # aristero menu
+    label_l_up = Label(label_left, image=render2, text="ΦΩΤΟΓΤΑΦΙΑ\n ΠΑΡΜΕΝΊΔΗΣ", borderwidth=1, highlightthickness=0, bg="gray26")  # photo parmenidi
+    label_l_down = Label(label_left, borderwidth=1, highlightthickness=0,bg="gray26")  # button gia menu kai alla frames
+
+    label_right = Label(intro_Frame, borderwidth=1, highlightthickness=0, bg="floral white")  # dexio menu
+    label_r_up = Label(label_right, borderwidth=1, highlightthickness=0, bg="floral white")  # panw meros me perilipsh kai hmerologio
+    label_ru_up = Label(label_r_up, borderwidth=1, highlightthickness=0,bg="floral white")  # kalos orisate+ to eniaio susthma klp
+    label_ruu_up = Label(label_ru_up, text="Καλώς ήρθατε στον Παρμενίδη!\n", borderwidth=0, highlightthickness=0,bg="floral white", font=("Calibri", 24, "bold"))
+    label_ruu_down = Label(label_ru_up, text="Το ενιαίο σύστημα για τις Πανελλήνιες Εξετάσεις.\n", borderwidth=0,highlightthickness=0, bg="floral white", font=("Calibri", 18))
+
+    # isws na 8elei up kai oxi left
+    label_ru_left = Label(label_ru_up, borderwidth=1, highlightthickness=0,bg="floral white")  # foto parmenidi+ desription parmenidi
+    label_rul_left = Label(label_ru_left, image=render1, borderwidth=1, highlightthickness=0,bg="floral white")  # foto parmenidi
+    label_rul_right = Label(label_ru_left, borderwidth=1, highlightthickness=0,bg="floral white")  # Ο παρμενιδης ειναι bla bla
+
+    label_ru_right = Label(label_ru_up, borderwidth=0, highlightthickness=1,bg="floral white")  # hmerologio gramma + hmerologio
+    label_rur_up = Label(label_ru_right, text="Ημερολόγιο\n", borderwidth=1, highlightthickness=0, bg="floral white",font=("Calibri", 18))  # hmerologio gramma
+    label_rur_down = Label(label_ru_right, borderwidth=1, highlightthickness=0, bg="floral white")  # hmerologio
+
+    label_r_down = Label(label_right, borderwidth=20, highlightthickness=0, relief="raised", bg="floral white")  # sunoptiko profil
+    label_rd_up = Label(label_r_down, text=" Συνοπτικό προφίλ: ",relief="groove", borderwidth=1, highlightthickness=0, bg="floral white",font=("Calibri", 18, "bold"))  # sunoptiko profil
+    label_rd_down = Label(label_r_down, borderwidth=1, highlightthickness=0, bg="floral white")  # onoma xrhsth kai alla
+    label_rdd_left = Label(label_rd_down,image=render3, borderwidth=0, highlightthickness=0, bg="floral white")  # eikona user
+    label_rdd_right = Label(label_rd_down, borderwidth=1, highlightthickness=0,bg="floral white")  # onoma xrhsth
+
+    
+    
+
+    descriptionText = Text(label_rul_right, height=8, bg="floral white", fg="gray44", borderwidth=0, highlightthickness=2,font=("Calibri", 12), width=55)
+    descriptionText.insert(INSERT,"Ο Παρμενίδης ήταν αρχαίος έλληνας φιλόσοφος. Γεννήθηκε στην \nΕλέα της Μεγάλης Ελλάδας στα τέλη του 6ου αιώνα π.Χ., σε ένα \nπεριβάλλον επηρεασμένο από τις απόψεις του Πυθαγόρα και του Ξενοφάνη. Ο Παρμενίδης υποστήριξε ότι η πολλαπότητα των \nυπάρχοντων πραγμάτων, οι μεταβαλλόμενες μορφές και η κίνηση τους δεν είναι παρά μια εμφάνιση μιας ενιαίας αιώνιας \nπραγματικότητα (<<Όν>>), οδηγώντας έτσι στην παρμενίδια αρχή \nότι <<όλα είναι ένα>>.")
+    descriptionText.config(state=DISABLED)#to be un editable
+
+
+
+
+
     label_left = Label(menu_Frame, bg="gray26",font=("Calibri", 24, "bold"))  # aristero menu
     label_l_up = Label(label_left, image=render2, borderwidth=1, highlightthickness=0, bg="gray26")  # photo parmenidi
     label_l_down = Label(label_left, borderwidth=1, highlightthickness=0,bg="gray26")  # button gia menu kai alla frames
@@ -67,7 +125,35 @@ def main():
     raiseNdrop_frame(all_Frame,none)
     raiseNdrop_frame(menu_Frame,none)
     raiseNdrop_frame(intro_Frame,none)
+
+
+########################################################################################################
+
+GradingCenterSubmission_all=Label(GradingCenterSubmission,bg="floral white")
+
+    GradingCenterSubmission_all = Label(GradingCenterSubmission_mid_top, bg="floral white")
+    GradingCenterSubmission_all = Label(GradingCenterSubmission, text="Υποβολή Λίστας: ",  bg="floral white",font=("Times New Roman (Times)", 18, "bold"),fg="black")
+    GradingCenterSubmission_all =  Label(lGradingCenterSubmission, bg="floral white",font=("Times New Roman (Times)", 18, "bold"),fg="blue",textvariable=folder_path_form)# η μεταβλητη εχει οριστει πανω απο την συναρτηση που καλειται browse_form
+    #pdf selected from user browse
+
+def confirm_choice():
+        msg_confirmation = messagebox.askquestion('Επιβεβαίωση!', 'Είστε σίγουροι ότι θέλετε να κάνετε υποβολή δήλωσης με αυτά τα στοιχεία;',icon='warning')
+        if msg_confirmation == 'yes':
+            messagebox.showinfo('Oλοκλήρωση', 'Η λίστα καταχωρήθηκε με επιτυχία!')
+            date_save=date_val.get()
+            month_save=month_val.get()
+            year_save=year_val.get()
+            print(date_save)
+            print(month_save)
+            print(year_save)
+        else:
+            messagebox.showinfo('Αποτυχία', 'Αποτυχία καταχώρησης λίστας!')
+
+
+
+##### AUTO EINAI TO TELEUTAIO KOMMATI TOU KWDIKA
     main_window.mainloop()
+
 
 def raiseNdrop_frame(frameUp,frameDown):
     global frame_counter
