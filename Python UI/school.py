27471079d2f06439549f6b9c6d7d1d85b0627d82
Stelios Vazaios <49122833+Vazeagle@users.noreply.github.com>
2020-05-25 02:59:01 +0300

    created  create/edit list page of students+email password

--- Python UI/school.py
+++ Python UI/school.py
@@ -1,3 +1,5 @@
+#extra need yagmail from pip import
+
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
@@ -6,6 +8,8 @@ from PIL import Image, ImageTk, ImageDraw, ImageFilter
 import pyautogui
 from datetime import datetime
 from tkcalendar import Calendar, DateEntry
+import uuid
+import yagmail
 
 
 
@@ -22,6 +26,8 @@ none="none" # προσωρινο για μεταβαση σε frames
 previous_frame="previous_frame"
 frame_counter=0
 init_pass=0
+stoixeia=[]#sos sos delete ειναι σαν temp για εισαγωγή data για email για έναν χρήστη
+
 
 #Frames For Main Window
 frame_temp=Frame()#Frame to get as temp to successfull change between frames
@@ -274,7 +280,7 @@ def main():
 
     #pack-εμφάνιση στοιχείων
     student_reg_all.pack(side = TOP, fill=BOTH, expand=1)
-    student_reg_all_top.pack(side = TOP, fill=X, ipady=50)
+    student_reg_all_top.pack(side = TOP, fill=X, ipady=30)
     student_reg_at_top.pack(side = TOP)
     student_reg_all_mid.pack(side = TOP, fill=BOTH, expand=1, pady=50)
     student_reg_am_top.pack(side = TOP)
@@ -284,9 +290,9 @@ def main():
     #student_reg_ambtl_top.pack(side = TOP, pady=50)
     #student_reg_ambtl_bot.pack(side = TOP)
     #Buttons pack
-    btn_reg_create.pack(side = TOP,pady=100)
+    btn_reg_create.pack(side = TOP,pady=50)
     btn_reg_edit.pack(side = TOP)
-    btn_reg_spectate.pack(side = TOP,pady=100)
+    btn_reg_spectate.pack(side = TOP,pady=50)
 
     #---------------------------------------------------------------------------------------------------
     #---------------------------------------------------------------------------------------------------
@@ -296,7 +302,212 @@ def main():
     #---------------------------------------------------------------------------------------------------
     #school_std_reg_create_Frame
 
+    std_reg_create_all = Label(school_std_reg_create_Frame, borderwidth=0, highlightthickness=0, bg="floral white")  # dexia arxikh selida
+    std_reg_create_all_top = Label(std_reg_create_all, text='Δημιουργία Λίστας Εγγραφών',borderwidth=1, highlightthickness=0, bg="floral white",font=("Times New Roman (Times)", 36, "bold"))
+    std_reg_create_all_bot = Label(std_reg_create_all, borderwidth=1, highlightthickness=0, bg="red")
+    std_reg_create_ab_top = Label(std_reg_create_all_bot, borderwidth=1, highlightthickness=0, bg="green")#include user name
 
+    #ONOMA LISTAS
+    std_reg_create_abt_top = Label(std_reg_create_ab_top, borderwidth=1, highlightthickness=0, bg="black")#LIST  name
+    #ΣΤΟΙΧΕΙΑ ΜΑΘΗΤΗ
+    std_reg_create_abt_top1 = Label(std_reg_create_ab_top, borderwidth=1, highlightthickness=0, bg="black")#student  name 
+    std_reg_create_abt_top2 = Label(std_reg_create_ab_top, borderwidth=1, highlightthickness=0, bg="black")#student  lastname 
+    std_reg_create_abt_top3 = Label(std_reg_create_ab_top, borderwidth=1, highlightthickness=0, bg="black")#student email 
+    std_reg_create_abt_top4 = Label(std_reg_create_ab_top, borderwidth=1, highlightthickness=0, bg="black")#student phone 
+    std_reg_create_abt_top5 = Label(std_reg_create_ab_top, borderwidth=1, highlightthickness=0, bg="black")#info
+
+    std_reg_create_abtltl_l = Label(std_reg_create_abt_top, text='Όνομα Λίστας*:\t',borderwidth=1, highlightthickness=0, bg="floral white",font=("Times New Roman (Times)", 18, "bold"))
+    std_reg_create_abtltl_l1 = Label(std_reg_create_abt_top1, text='Όνομα*:\t\t',borderwidth=1, highlightthickness=0, bg="floral white",font=("Times New Roman (Times)", 18, "bold"))
+    std_reg_create_abtltl_l2 = Label(std_reg_create_abt_top2, text='Επώνυμο*:\t',borderwidth=1, highlightthickness=0, bg="floral white",font=("Times New Roman (Times)", 18, "bold"))
+    std_reg_create_abtltl_l3 = Label(std_reg_create_abt_top3, text='E-mail*:\t\t',borderwidth=1, highlightthickness=0, bg="floral white",font=("Times New Roman (Times)", 18, "bold"))
+    std_reg_create_abtltl_l4 = Label(std_reg_create_abt_top4, text='Τηλέφωνο:\t',borderwidth=1, highlightthickness=0, bg="floral white",font=("Times New Roman (Times)", 18, "bold"))
+    std_reg_create_abtltl_l5 = Label(std_reg_create_abt_top5, text='(*Υποχρεωτικά πεδία)',borderwidth=1, highlightthickness=0, bg="floral white",font=("Times New Roman (Times)", 12, "bold"))
+    
+    #TEXTS AS INPUTS!!!!!!!!!!!
+    std_reg_create_abtltl_r = Text(std_reg_create_abt_top, bg="WHITE", height=1, width=40, fg="black", borderwidth=1, highlightthickness=2,font=("Calibri", 16))
+    std_reg_create_abtltl_r1 = Text(std_reg_create_abt_top1, bg="WHITE", height=1, width=40, fg="black", borderwidth=1, highlightthickness=2,font=("Calibri", 16))
+    std_reg_create_abtltl_r2 = Text(std_reg_create_abt_top2, bg="WHITE", height=1, width=40, fg="black", borderwidth=1, highlightthickness=2,font=("Calibri", 16))
+    std_reg_create_abtltl_r3 = Text(std_reg_create_abt_top3, bg="WHITE", height=1, width=40, fg="black", borderwidth=1, highlightthickness=2,font=("Calibri", 16))
+    std_reg_create_abtltl_r4 = Text(std_reg_create_abt_top4, bg="WHITE", height=1, width=40, fg="black", borderwidth=1, highlightthickness=2,font=("Calibri", 16))
+    std_reg_create_abtltl_r1.config(state=DISABLED)
+    std_reg_create_abtltl_r2.config(state=DISABLED)
+    std_reg_create_abtltl_r3.config(state=DISABLED)
+    std_reg_create_abtltl_r4.config(state=DISABLED)
+
+    #Δήλωση textvariables sos πριν το καλεσμα τους
+    register_list = StringVar()#(label_Statement1_all_mt8l_left)
+    register_list.set("")
+
+    std_reg_create_ab_mid = Label(std_reg_create_all_bot, borderwidth=1, highlightthickness=0, bg="blue")#include announcements
+    std_reg_create_abm_top = Label(std_reg_create_ab_mid, borderwidth=1, highlightthickness=0, bg="blue")
+    std_reg_create_abmt_left = Label(std_reg_create_abm_top, text='Λίστα Εγγραφών: ',borderwidth=1, highlightthickness=0, bg="floral white",font=("Times New Roman (Times)", 18, "bold"))#announcement title
+    std_reg_create_abmt_right = Label(std_reg_create_abm_top,textvariable=register_list ,borderwidth=1, highlightthickness=0, bg="floral white",font=("Times New Roman (Times)", 18, "bold"))#announcement title
+    std_reg_create_abm_bot = Label(std_reg_create_ab_mid, bg="cyan2")#announcement box
+
+    std_reg_create_ab_bot = Label(std_reg_create_all_bot, borderwidth=1, highlightthickness=0, bg="red")#ΚΑΤΩ ΠΛΕΥΡΑ ΜΕ ΚΟΥΜΠΙΑ NEXT KAI RETURN
+    
+    
+
+
+    def confirm_list_name():
+        list_name = std_reg_create_abtltl_r.get('1.0', 'end-1c')
+        if (list_name!=""):#Αν δεν ειναι κενο το input
+            std_reg_create_abtltl_r.config(state=DISABLED)
+            register_list.set(list_name)
+            std_reg_create_abtltl_r1.config(state=NORMAL)
+            std_reg_create_abtltl_r2.config(state=NORMAL)
+            std_reg_create_abtltl_r3.config(state=NORMAL)
+            std_reg_create_abtltl_r4.config(state=NORMAL)
+
+            btn_reg_create_return.config(state=NORMAL)
+            btn_reg_create_confirm.config(state=NORMAL)
+            btn_reg_list_name.config(state=DISABLED)
+            btn_reg_delete_list.config(state=NORMAL)
+            btn_reg_add.config(state=NORMAL)
+            btn_reg_delete.config(state=NORMAL)
+        else:
+            messagebox.showinfo('Σφάλμα', 'Παρακαλώ δημιουργείστε μία λίστα για τα στοιχεία των μαθητών!',icon='warning')
+        print(list_name)
+        print("register_list=",register_list)
+
+    def delete_list():
+        print("deleted list")
+    
+
+    def add_user():
+        global stoixeia
+        std_name = std_reg_create_abtltl_r1.get('1.0', 'end-1c')    #αντι end-1c αν βαλεις σκετο end βαζει στο τέλος newline 
+        std_lastname = std_reg_create_abtltl_r2.get('1.0', 'end-1c')
+        std_email = std_reg_create_abtltl_r3.get('1.0', 'end-1c')
+        std_phone = std_reg_create_abtltl_r4.get('1.0', 'end-1c')
+        if (std_name!="" and std_lastname!="" and std_email!=""):
+            std_name_ok=std_name
+            std_lastname_ok=std_lastname
+            std_email_ok=std_email
+            std_phone_ok=std_phone
+            #insert ola ta ok se ΒΔ τα ok σαν attributes
+            password = uuid.uuid4().hex[:10]#random unique 10 digit password will be send via email or phone number
+
+            ###################################to delete just test
+            stoixeia.append(std_name_ok)
+            stoixeia.append(std_lastname_ok)
+            stoixeia.append(password)
+            stoixeia.append(std_email_ok)
+            stoixeia.append(std_phone_ok)
+            print(stoixeia)
+            #--------------------------------------------------
+        else:
+            messagebox.showinfo('Σφάλμα', 'Παρακαλώ εισάγετε ορθά τα στοιχεία του μαθητή',icon='warning')
+
+        print(std_name_ok)
+        print(std_lastname_ok)
+        print(std_email_ok)
+        print(std_phone_ok)
+        print("password",password) 
+        #https://realpython.com/python-send-email/
+        #sos sos zisis ολα αυτα τα στοιχεια θα δημιουργουνται για την κλαση λιστας και οταν γίνει επιβεβαίωση λιστας θα αποθηκεύεται
+        #  και θα διαβαζει ολους τους μαθητες στην λίστα και θα στέλνει από εκει emails.
+        
+
+    def delete_user():
+        print("deleted user")
+
+    def confirm_registry():
+        #send email to all students
+        #εδω θελουμε μια λίστα που να έχει αποθηκεύσει τα στοιχεία των μαθητών να την τρέχει και να στέλνει email.
+        #https://dev.to/davidmm1707/how-to-send-emails-with-just-a-few-lines-of-code-with-yagmail-in-python-25pm
+        sender_email = "parmenidis.gr@gmail.com"
+        sender_password = "tl2020Parme"
+        try:
+            yag = yagmail.SMTP(user=sender_email, password=sender_password)
+            subject='Στοιχεία Χρήστη Για Το Σύστημα Παρμενίδης'
+            e1="Όνομα: " + stoixeia[0]
+            e2="Επώνυμο: " + stoixeia[1]
+            e3="Κωδικός Πρόσβασης: " + stoixeia[2]
+            e4="E-mail: " + stoixeia[3]
+            e5="Αριθμός Επικοινωνίας: " + stoixeia[4]
+            contents = ["Καλώς ορίσατε στο ενιαίο Σύστημα Πανελληνίων Παρμενίδης!","Τα στοιχεία σας είνια τα εξής",e1,e2,e3,e4,e5]
+            reciever=stoixeia[3]
+            yag.send(reciever, subject, contents)
+        except:
+            print("Error, email was not sent")
+        print("registry confirmed")
+
+    #ORISMOS BTNS
+    btn_reg_create_return = Button(std_reg_create_ab_bot, text="Επιστροφή", command=lambda: raiseNdrop_frame(school_std_reg_Frame,previous_frame), bg="red3",height = 1, width = 12,font=("Calibri", 16, "bold"))
+    btn_reg_create_confirm = Button(std_reg_create_ab_bot, text="Επιβεβαίωση", state=DISABLED, command=lambda: confirm_registry(), bg="red3",font=("Calibri", 16, "bold"))
+    btn_reg_list_name = Button(std_reg_create_abt_top, text="Δημιουργία Λίστας", command=lambda: confirm_list_name(), bg="red3",font=("Calibri", 16, "bold"))
+    btn_reg_delete_list = Button(std_reg_create_ab_bot, text="Διαγραφή Λίστας", state=DISABLED, command=lambda: delete_list(), bg="red3",font=("Calibri", 16, "bold"))
+    btn_reg_add = Button(std_reg_create_ab_top, text="Προσθήκη Εγγραφής", state=DISABLED, command=lambda: add_user(), bg="red3",font=("Calibri", 16, "bold"))
+    btn_reg_delete = Button(std_reg_create_ab_top, text="Διαγραφή Εγγραφής", state=DISABLED, command=lambda: delete_user(), bg="red3",font=("Calibri", 16, "bold"))
+
+    #orismos listbox sos sos edw 8elei tkinterctrl
+    user_list  = Listbox (std_reg_create_abm_bot, bg="floral white", borderwidth=2, highlightthickness=0, selectmode='single')
+    
+    scrollh_usr = Scrollbar(std_reg_create_abm_bot, orient="horizontal", command=user_list.xview)
+    scrollv_usr= Scrollbar(std_reg_create_abm_bot, orient="vertical", command=user_list.yview)
+    std_reg_create_abm_bot.bind("<Configure>",lambda e: user_list.configure(scrollregion=user_list.bbox("all")))
+    user_list.configure(yscrollcommand=scrollv_usr.set, xscrollcommand=scrollh_usr.set, font=("Times New Roman (Times)", 16,"bold"))
+    std_reg_create_abm_bot.bind("<MouseWheel>", scrollv_usr)#ΚΑΘΕΤΟ SCROLL ΜΕ ΡΟΔΑ ΠΟΝΤΙΚΙΟΥ
+    
+    #add elements to announcements
+    user_list.insert(1, "This is a test to see if the announcements works as it should be")
+    user_list.insert(2, "Ανακοινωση Ημερομηνίας Δηλώσεων")
+    user_list.insert(3, "Πρόγραμμα εξεταστικής έτους 2020-2021")
+    user_list.insert(4, "#ΜΕΝΟΥΜΕ_ΣΠΙΤΙ")
+    user_list.insert(5, "Έναρξη δηλώσεων μαθητών")
+    user_list.insert(6, "ΠΑΡΜΕΝΙΔΗΣ ΜΕ TKINTER ΓΙΑ GUI")
+    user_list.insert(7, "Πεισσότερες Ανακοινώσεις για να δούμε σε πράξη το κάθετο scroll και το horizontal scroll")
+
+    #packs εμφάνιση στοιχείων
+    std_reg_create_all.pack(side=TOP,fill=BOTH,expand=1)#δεξιο μενου-αρχικη σελίδα
+    std_reg_create_all_top.pack(side=TOP,fill=X)
+
+    std_reg_create_all_bot.pack(side=TOP, expand=1, fill=BOTH,pady=30)
+    std_reg_create_ab_top.pack(side=TOP, fill=X)#expand=1
+    
+    #αριστερη πλευρα με  στοιχεια εγγραφης
+    std_reg_create_abt_top.pack(side=TOP, fill=X)
+    std_reg_create_abt_top1.pack(side=TOP, fill=X)
+    std_reg_create_abt_top2.pack(side=TOP, fill=X)
+    std_reg_create_abt_top3.pack(side=TOP, fill=X)
+    std_reg_create_abt_top4.pack(side=TOP, fill=X)
+    std_reg_create_abt_top5.pack(side=TOP, fill=X)
+
+    std_reg_create_abtltl_l.pack(side=LEFT, padx=50)#ονομα λίστας
+    std_reg_create_abtltl_l1.pack(side=LEFT, padx=50)#ονομα
+    std_reg_create_abtltl_l2.pack(side=LEFT, padx=50)#επωνυμο
+    std_reg_create_abtltl_l3.pack(side=LEFT, padx=50)#e-mail
+    std_reg_create_abtltl_l4.pack(side=LEFT, padx=50)#τηλ επικ
+    std_reg_create_abtltl_l5.pack(side=LEFT, padx=50)#info
+
+    std_reg_create_abtltl_r.pack(side=LEFT)#ονομα λίστας
+    std_reg_create_abtltl_r1.pack(side=LEFT)#ονομα
+    std_reg_create_abtltl_r2.pack(side=LEFT)#επωνυμο
+    std_reg_create_abtltl_r3.pack(side=LEFT)#e-mail
+    std_reg_create_abtltl_r4.pack(side=LEFT)#τηλ επικ
+
+    #κατω πλευρα με Λίστα
+    std_reg_create_ab_mid.pack(side=TOP, fill=BOTH,expand=1)#,ipady=100)#, expand=1, fill=X)#BOTH
+    std_reg_create_abm_top.pack(side=TOP, fill=X)
+    std_reg_create_abmt_left.pack(side=LEFT,padx=10)
+    std_reg_create_abmt_right.pack(side=LEFT)
+    std_reg_create_abm_bot.pack(side=TOP, expand=1, fill=BOTH)#BOTH
+    scrollv_usr.pack(side=RIGHT, fill=Y)
+    scrollh_usr.pack(side=BOTTOM, fill=X)
+    user_list.pack(side=BOTTOM, expand=1, fill=BOTH)#BOTH
+    #std_reg_create_abm_bot.pack(side=BOTTOM, expand=1, fill=BOTH)#BOTH
+
+    #κατω πλευρα με buttons σελιδας
+    std_reg_create_ab_bot.pack(side= TOP, fill=X)
+
+    #buttons MENU
+    btn_reg_create_confirm.pack(side=RIGHT, padx=50)
+    btn_reg_create_return.pack(side=RIGHT)
+    btn_reg_add.pack(side=RIGHT, padx=50)
+    btn_reg_delete.pack(side=RIGHT) 
+    btn_reg_list_name.pack(side=LEFT, padx=30) 
+    btn_reg_delete_list.pack(side=LEFT, padx=50) 
     #---------------------------------------------------------------------------------------------------
     #---------------------------------------------------------------------------------------------------
     #---------------------------------------------------------------------------------------------------
