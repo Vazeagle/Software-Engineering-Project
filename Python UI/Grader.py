5c463140cafee3b4c226dd4db579fb8ada4fab7a
AnastsasiaSvi <anastasia.sviggou@gmail.com>
2020-05-28 20:08:50 +0300

    created page and functions for grader Info

--- Python UI/Grader.py
+++ Python UI/Grader.py
@@ -27,14 +27,14 @@ none="none" #για μεταβαση σε frames
 previous_frame="previous_frame"
 frame_counter=0
 init_pass=0
-
+temp_image= None
 
 #Frames For Main Window
 frame_temp=Frame()#Frame to get as temp to successfull change between frames
 grader_Frame=Frame(main_window, bg="white")
 grader_menu_Frame=Frame(grader_Frame, bg="gray26")
 grader_intro_Frame = Frame(grader_Frame, bg="floral white")
-grader_graderInfo_Frame = Frame(grader_Frame, bg="floral white") 
+graderInfo_Frame = Frame(grader_Frame, bg="floral white") 
 grader_pexams_Frame = Frame(grader_Frame, bg="floral white") #Πανελλήνιες
 grader_grading_Frame=Frame(grader_Frame, bg="floral white") #εκχώρηση βαθμολογιών  
 grader_problems_Frame = Frame(grader_Frame, bg="floral white") #προβλήματα με την εφαρμογή
@@ -82,6 +82,10 @@ load2 = Image.open('P2.gif')
 load2 = load2.resize((100, 100), Image.ANTIALIAS)
 render2 = ImageTk.PhotoImage(load2)
 
+load3 = Image.open('profil.jpg')
+load3 = load3.resize((100, 100), Image.ANTIALIAS)
+render3 = ImageTk.PhotoImage(load3)
+
 def main():
 
     grader_menu = Label(grader_menu_Frame, bg="gray26",font=("Times New Roman (Times)", 24, "bold"))  # aristero menu
@@ -128,7 +132,7 @@ def main():
 
     #orismos buttons
     btn_next0 = Button(gmenu_l_down, text="Αρχική Σελίδα", command=lambda: raiseNdrop_frame(grader_intro_Frame,previous_frame), bg="gray26",height = 2, width = 35,font=("Calibri", 14, "bold"))
-    btn_next1 = Button(gmenu_l_down, text="Στοιχεία Χρήστη", command=lambda: raiseNdrop_frame(grader_graderInfo_Frame,previous_frame), bg="gray26",height = 2, width = 35,font=("Calibri", 14, "bold"))
+    btn_next1 = Button(gmenu_l_down, text="Στοιχεία Χρήστη", command=lambda: raiseNdrop_frame(graderInfo_Frame,previous_frame), bg="gray26",height = 2, width = 35,font=("Calibri", 14, "bold"))
     btn_next2 = Button(gmenu_l_down, text="Πανελλήνιες", command=lambda: raiseNdrop_frame(grader_pexams_Frame,previous_frame), bg="gray26",height = 2, width = 35,font=("Calibri", 14, "bold")) 
     btn_next3 = Button(gmenu_l_down, text="Προβλήματα", command=lambda: raiseNdrop_frame(grader_pexams_Frame,previous_frame), bg="gray26",height = 2, width = 35,font=("Calibri", 14, "bold")) 
     btn_next4 = Button(gmenu_l_down, text="Έξοδος", command=lambda: ExitApp(), bg="gray26",height = 2, width = 35,font=("Calibri", 14, "bold"))
@@ -143,7 +147,7 @@ def main():
     initialGrader_all_top.pack(side=TOP,fill=X,pady=50)
     initialGrader_all_bot.pack(side=LEFT, expand=1, fill=BOTH)
     initialGrader_ab_top.pack(side=TOP, expand=1, fill=BOTH, ipady=100)
-    #αριστερη πλευρα με ονομα σχολειου-χρηστη
+    #αριστερη πλευρα με ονομα βαθμολογητή
     initialGrader_abt_left.pack(side=LEFT, fill=Y, padx=50)
     initialGrader_abtl_top.pack(side=TOP, fill=X)
     initialGrader_abtlt_left.pack(side=LEFT)
@@ -180,6 +184,180 @@ def main():
     ministry_news_list.insert(5, "Έναρξη δηλώσεων μαθητών")
     ministry_news_list.insert(6, "ΠΑΡΜΕΝΙΔΗΣ ΜΕ TKINTER ΓΙΑ GUI")
     ministry_news_list.insert(7, "Πεισσότερες Ανακοινώσεις για να δούμε σε πράξη το κάθετο scroll και το horizontal scroll")
+
+
+
+
+    #---------------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------------
+    #---------------------------------------------------------------------------------------------------
+    #graderInfo_Frame
+
+    graderinfo_all = Label(graderInfo_Frame, bg="floral white")
+    graderinfo_a_top = Label(graderinfo_all, bg="floral white", text="Πληροφορίες Χρήστη",font=("Times New Roman (Times)", 36, "bold"),fg="dodger blue")
+    graderinfo_a_mid = Label(graderinfo_all, bg="floral white")#info
+    graderinfo_a_bot = Label(graderinfo_a_mid, bg="floral white")# buttons
+
+    graderinfo_am_general = Label(graderinfo_a_mid, bg="floral white")
+    graderinfo_am_gen_title = Label(graderinfo_am_general, bg="SkyBlue1", text="Γενικά:",font=("Times New Roman (Times)", 18, "bold"),fg="black")
+    graderinfo_am_gen_info = Label(graderinfo_am_general, bg="floral white")# containt text and photo
+
+    graderinfo_am_gen_info_left = Label(graderinfo_am_gen_info, bg="floral white")#text info container
+    #id
+    graderinfo_id = Label(graderinfo_am_gen_info_left, bg="floral white")
+    graderinfo_id_left = Label(graderinfo_id, bg="floral white", text="ID Χρήστη:\t",font=("Times New Roman (Times)", 18, "bold"),fg="black")
+    graderinfo_id_right = Text(graderinfo_id, bg="WHITE", height=1, width=30, fg="black", borderwidth=1, highlightthickness=2,font=("Times New Roman (Times)", 16))
+    graderinfo_id_right.config(state=DISABLED)
+    
+    #password
+    graderinfo_password = Label(graderinfo_am_gen_info_left, bg="floral white")
+    graderinfo_pass_left = Label(graderinfo_password, bg="floral white", text="Κωδικός:\t",font=("Times New Roman (Times)", 18, "bold"),fg="black")
+    graderinfo_pass_right = Text(graderinfo_password, bg="WHITE", height=1, width=30, fg="black", borderwidth=1, highlightthickness=2,font=("Times New Roman (Times)", 16))
+    graderinfo_pass_right.config(state=DISABLED)
+
+    #fullname
+    graderinfo_fullname = Label(graderinfo_am_gen_info_left, bg="floral white")
+    graderinfo_fullname_left = Label(graderinfo_fullname, bg="floral white", text="Ονοματεπώνυμο:\t",font=("Times New Roman (Times)", 18, "bold"),fg="black")
+    graderinfo_fullname_right = Text(graderinfo_fullname, bg="WHITE", height=1, width=30, fg="black", borderwidth=1, highlightthickness=2,font=("Times New Roman (Times)", 16))
+    graderinfo_fullname_right.config(state=DISABLED)
+    
+    #school
+    graderinfo_school = Label(graderinfo_am_gen_info_left, bg="floral white")
+    graderinfo_school_left = Label(graderinfo_school, bg="floral white", text="Σχολείο:\t\t",font=("Times New Roman (Times)", 18, "bold"),fg="black")
+    graderinfo_school_right = Text(graderinfo_school, bg="WHITE", height=1, width=30, fg="black", borderwidth=1, highlightthickness=2,font=("Times New Roman (Times)", 16))
+    graderinfo_school_right.config(state=DISABLED)
+    
+    #photo
+    graderinfo_am_gen_info_right = Label(graderinfo_am_gen_info, bg="floral white")#photo info container
+    graderinfo_photo_top = Label(graderinfo_am_gen_info_right, bg="floral white", text="Εικόνα Προφιλ:",font=("Times New Roman (Times)", 18, "bold"),fg="black")
+    graderinfo_photo_mid = Label(graderinfo_am_gen_info_right, bg="floral white",image=render3)
+    graderinfo_photo_bot = Label(graderinfo_am_gen_info_right, bg="floral white")#button change invisible
+
+    #contact information
+    graderinfo_am_contact_info = Label(graderinfo_a_mid, bg="floral white")
+    graderinfo_contact = Label(graderinfo_am_contact_info, bg="SkyBlue1", text="Επικοινωνία:",font=("Times New Roman (Times)", 18, "bold"),fg="black")
+    graderinfo_contact_all = Label(graderinfo_am_contact_info, bg="floral white")#contain email and number
+    
+    #email
+    graderinfo_contact_email = Label(graderinfo_contact_all, bg="floral white")#email container
+    graderinfo_contact_email_left = Label(graderinfo_contact_email, bg="floral white", text="E-mail:\t\t",font=("Times New Roman (Times)", 18, "bold"),fg="black")
+    graderinfo_contact_email_right = Text(graderinfo_contact_email, bg="WHITE", height=1, width=30, fg="black", borderwidth=1, highlightthickness=2,font=("Times New Roman (Times)", 16))
+    graderinfo_contact_email_right.config(state=DISABLED)
+   
+    #number
+    graderinfo_contact_number = Label(graderinfo_contact_all, bg="floral white")#contain number
+    graderinfo_contact_number_left = Label(graderinfo_contact_number, bg="floral white", text="Αριθμός Τηλεφώνου:",font=("Times New Roman (Times)", 18, "bold"),fg="black")
+    graderinfo_contact_number_right = Text(graderinfo_contact_number, bg="WHITE", height=1, width=30, fg="black", borderwidth=1, highlightthickness=2,font=("Times New Roman (Times)", 16))
+    graderinfo_contact_number_right.config(state=DISABLED)
+
+    btn_grader_info_edit = Button(graderinfo_a_bot, text="Επεξεργασία", command=lambda: edit_profile(), bg="floral white",font=("Times New Roman (Times)", 14, "bold"),height=1 ,width=12)
+    btn_grader_save = Button(graderinfo_a_bot, state=DISABLED,text="Αποθήκευση", command=lambda: save_profile(), bg="floral white",font=("Times New Roman (Times)", 14, "bold"),height=1 ,width=12)
+    btn_grader_pic_edit = Button(graderinfo_photo_bot, state=DISABLED, text="Αλλαγή Εικόνας", command=lambda: edit_pic(), bg="floral white",font=("Times New Roman (Times)", 14, "bold"),height=1 ,width=12)
+    
+    #χρειαζόμαστε συνάρτηση για τα αρχικά Data sos
+    def edit_pic():
+        global temp_image
+        path=filedialog.askopenfilename(filetypes=[("Image File",'.jpg')])
+        im = Image.open(path)
+        im = im.resize((100, 100), Image.ANTIALIAS)
+        render3 = ImageTk.PhotoImage(im)
+        graderinfo_photo_mid.configure(image=render3)
+        graderinfo_photo_mid.image=render3
+        temp_image = render3
+
+    def edit_profile():
+        #kane editable ta Text 
+        graderinfo_id_right.config(state=DISABLED)
+        graderinfo_pass_right.config(state=NORMAL)
+        graderinfo_fullname_right.config(state=DISABLED)
+        graderinfo_school_right.config(state=DISABLED)
+        graderinfo_contact_email_right.config(state=NORMAL)
+        graderinfo_contact_number_right.config(state=NORMAL)
+        btn_grader_pic_edit.config(state=NORMAL)
+        btn_grader_save.config(state=NORMAL)
+        
+        #Emfanise duo koympia stis 8eseis pou prepei cancel kai save
+        #pop-ups gia la8oi xrhsth
+        #old password confirmation
+        #password_btn)change
+        
+        print("edit")
+
+        
+    def save_profile():
+        
+        global temp_image #Αποθηκευση Φωτογραφίας στη ΒΔ
+        passw = graderinfo_pass_right.get("1.0",'end-1c')
+        email = graderinfo_contact_email_right.get("1.0",'end-1c')
+        phone = graderinfo_contact_number_right.get("1.0",'end-1c')
+
+        btn_grader_save.config(state=DISABLED)
+        btn_grader_pic_edit.config(state=DISABLED)
+        graderinfo_pass_right.config(state=DISABLED)
+        graderinfo_contact_email_right.config(state=DISABLED)
+        graderinfo_contact_number_right.config(state=DISABLED)
+
+        print("save")
+
+    #pack reveal
+    #general labels
+    graderinfo_all.pack(side=TOP, expand=1, fill=BOTH)
+    graderinfo_a_top.pack(side=TOP)
+    graderinfo_a_mid.pack(side=LEFT, expand=1, fill=BOTH, pady=50)
+    graderinfo_a_bot.pack(side=BOTTOM, fill=X)
+    
+    #grader
+    graderinfo_am_general.pack(side=TOP, expand=1, fill=BOTH)
+    graderinfo_am_gen_title.pack(side=TOP, fill=X)
+    graderinfo_am_gen_info.pack(side=TOP, fill=X)
+    graderinfo_am_gen_info_left.pack(side=LEFT)
+   
+    #id
+    graderinfo_id.pack(side=TOP, fill=X)
+    graderinfo_id_left.pack(side=LEFT)
+    graderinfo_id_right.pack(side=LEFT)
+    
+   
+    #password
+    graderinfo_password.pack(side=TOP, fill=X)
+    graderinfo_pass_left.pack(side=LEFT)
+    graderinfo_pass_right.pack(side=LEFT)
+   
+    #fullname
+    graderinfo_fullname.pack(side=TOP, fill=X)
+    graderinfo_fullname_left.pack(side=LEFT)
+    graderinfo_fullname_right.pack(side=LEFT)
+    
+    #school
+    graderinfo_school.pack(side=TOP, fill=X)
+    graderinfo_school_left.pack(side=LEFT)
+    graderinfo_school_right.pack(side=LEFT)
+    
+    #photo
+    graderinfo_am_gen_info_right.pack(side=RIGHT,ipadx=100, fill=Y)
+    graderinfo_photo_top.pack(side=TOP)
+    graderinfo_photo_mid.pack(side=TOP)
+    graderinfo_photo_bot.pack(side=TOP)
+   
+    #contact information
+    graderinfo_am_contact_info.pack(side=TOP, expand=0, fill=X)
+    graderinfo_contact.pack(side=TOP, fill=X)
+    graderinfo_contact_all.pack(side=LEFT, expand=1, fill=BOTH)
+    #email
+    graderinfo_contact_email.pack(side=TOP, fill=X)
+    graderinfo_contact_email_left.pack(side=LEFT)
+    graderinfo_contact_email_right.pack(side=LEFT)
+    #number
+    graderinfo_contact_number.pack(side=TOP, fill=X)
+    graderinfo_contact_number_left.pack(side=LEFT)
+    graderinfo_contact_number_right.pack(side=LEFT)
+    #button edit
+    btn_grader_info_edit.pack(side=RIGHT, padx=50)
+    btn_grader_save.pack(side=RIGHT)
+    btn_grader_pic_edit.pack()
 
     #ΑΡΧΙΚΑ FRAMES ΕΜΦΑΝΙΣΗ
     raiseNdrop_frame(grader_Frame,none)
