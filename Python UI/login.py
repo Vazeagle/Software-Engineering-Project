9aec354fd936f66ae514464977daeee0cd4b02d7
StormLord666 <stauropan@gmail.com>
2020-05-29 17:54:25 +0300

    login σελιδα ετοιμη

--- Python UI/login.py
+++ Python UI/login.py
@@ -67,25 +67,88 @@ month_options=[]
 date_options=[]
 year_options=[]
 
+    
 load2 = Image.open('P2.gif')
 load2 = load2.resize((100, 100), Image.ANTIALIAS)
 render2 = ImageTk.PhotoImage(load2)
 
 
+
+###frames
+frame_temp=Frame()#Frame to get as temp to successfull change between frames
 all_Frame=Frame(main_window, bg="white")
+menu_Frame=Frame(all_Frame, bg="gray26")
+intro_Frame = Frame(all_Frame, bg="floral white")
+application_Frame=Frame(all_Frame, bg="floral white")
+pending_applic_Frame=Frame(all_Frame, bg="floral white") #aitiseis
+applic_verify_Frame=Frame(all_Frame, bg="floral white")#pendingaitiseis
+department_application_Frame=Frame(all_Frame, bg="floral white")#dhlwsh thesewn
+statement_Frame4=Frame(all_Frame, bg="floral white")#dhmiourgia
+page_Frame=Frame(all_Frame, bg="floral white")#panellhnies
+page_edit_Frame=Frame(all_Frame, bg="floral white")
+capacity_Frame=Frame(all_Frame, bg="floral white")#programma
+capacity_submit_Frame=Frame(all_Frame, bg="floral white")#eksetastiko kentro
+graderslist_frame=Frame(all_Frame, bg="floral white") #vathmologites
+acceptdeny_frame=Frame(all_Frame, bg="floral white") #apodoxi aporripsi 
+processing_frame=Frame(all_Frame, bg="floral white") #epeksergasia
+problems_frame=Frame(all_Frame, bg="floral white") #provlimata
+exit_frame=Frame(all_Frame, bg="floral white") #eksodos
+
 
 def main():
-    label_top = Label(all_Frame, borderwidth=1, highlightthickness=0, bg="floral white")  # dexio menu
-    label_top1 = Label(label_top, borderwidth=1, highlightthickness=0, bg="floral white")  # panw meros me perilipsh kai hmerologio
-    label_top2 = Label(label_top1, borderwidth=1, highlightthickness=0,bg="floral white")  # kalos orisate+ to eniaio susthma klp
-    label_pic = Label(label_top2, image=render2, text="ΦΩΤΟΓΤΑΦΙΑ\n ΠΑΡΜΕΝΊΔΗΣ", borderwidth=1, highlightthickness=0, bg="gray26")
-    label_parmen = Label(label_top2, text="ΠΑΡΜΕΝΙΔΗΣ.\n", borderwidth=0,highlightthickness=0, bg="floral white", font=("Times New Roman (Times)", 25))
+    
+    login_all=Label(intro_Frame, borderwidth=1, highlightthickness=0, bg="floral white")
+    
+    label_top = Label(login_all, borderwidth=1, highlightthickness=0,bg="floral white")  # kalos orisate+ to eniaio susthma klp
+    label_pic = Label(label_top, image=render2, borderwidth=1, highlightthickness=0, bg="floral white")
+    label_parmen = Label(label_top, text="ΣΥΣΤΗΜΑ ΠΑΡΜΕΝΙΔΗΣ\n", borderwidth=0,highlightthickness=0, bg="floral white", font=("Times New Roman (Times)", 24, "bold"))
+    label_top3 = Label(login_all,bg="white", borderwidth=2, highlightthickness=2, relief="groove")
+
+    
+    login_usr = Label(label_top3, bg="white")
+    login_usr1 = Label(login_usr, text="Όνομα Χρήστη:",  bg="white",font=("Times New Roman (Times)", 16, "bold"),fg="black")
+    login_usr2 = Text(login_usr, bg="WHITE", height=1, width=40, fg="black", borderwidth=1, highlightthickness=2,font=("Times New Roman (Times)", 16))
+    
+    login_passw = Label(label_top3, bg="white")
+    login_passw1 = Label(login_passw, text="Κωδικός Πρόσβασης:",  bg="white",font=("Times New Roman (Times)", 16, "bold"),fg="black")
+    login_passw2 = Text(login_passw, bg="WHITE", height=1, width=40, fg="black", borderwidth=1, highlightthickness=2,font=("Times New Roman (Times)", 16))
+
+    label_buttons = Label(label_top3, bg="floral white")
+    btn_acccept = Button(label_buttons, text="Σύνδεση", bg="green3",font=("Times New Roman (Times)", 14, "bold"),height=1 ,width=12)
+    btn_exit = Button(label_buttons, text="Έξοδος", command=lambda: ExitApp(), bg="gray",font=("Calibri", 14, "bold"),height=1 ,width=12)
 
 
+    login_all.pack(side = TOP, fill=BOTH, expand=1)
 
+
+    label_top.pack(side=TOP,fill=X,expand=1)
+    label_pic.pack(side=TOP)
+    label_parmen.pack(side=TOP)
+    label_top3.pack(side=TOP,fill=Y,expand=1)
+
+    
+    login_usr.pack(side=TOP)
+    login_usr1.pack(side = LEFT, padx=10)
+    login_usr2.pack(side = LEFT, padx=10)
+    
+    login_passw.pack(side=TOP)
+    login_passw1.pack(side = LEFT, padx=10)
+    login_passw2.pack(side = LEFT, padx=10)
+
+    label_buttons.pack(side=TOP)
+    btn_acccept.pack(side = LEFT, padx=10)
+    btn_exit.pack(side = LEFT, padx=10)
+
+
+    raiseNdrop_frame(all_Frame,none)
+    raiseNdrop_frame(menu_Frame,none)
+    raiseNdrop_frame(intro_Frame,none)
+
+    
+    
+
+##### AUTO EINAI TO TELEUTAIO KOMMATI TOU KWDIKA
     main_window.mainloop()
 
 main()
-
-
 
