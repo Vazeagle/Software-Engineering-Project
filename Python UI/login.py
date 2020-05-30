ce5ccd3bb77e185496a6c22defeb1b1f2389dd8c
StormLord666 <stauropan@gmail.com>
2020-05-30 17:28:17 +0300

    login συνδεση με σωστες σελιδες (ανεπιτυχης)

--- Python UI/login.py
+++ Python UI/login.py
@@ -1,12 +1,17 @@
 import os, sys
+from board import *
+from department import *
+from Grader import *
+from initUI import *
+from school import *
 from Classes import *
 from Classes import Lesson,Orientation,Direction,Department,Student,School
 from tkinter import *
 from tkinter import filedialog
 from tkinter import messagebox
 from tkinter import ttk
-#from TkTreectrl import *
-#import TkTreectrl as treectrl
+from TkTreectrl import *
+import TkTreectrl as treectrl
 from PIL import Image, ImageTk, ImageDraw, ImageFilter
 import pyautogui
 from datetime import datetime
@@ -73,25 +78,27 @@ load2 = load2.resize((100, 100), Image.ANTIALIAS)
 render2 = ImageTk.PhotoImage(load2)
 
 
+n = 5
+m = 2
+arr = [[0] * m for i in range(n)]
+arr[0][0] = "vastyl"
+arr[0][1] = "24682468"
+arr[1][0] = "ceidupatras"
+arr[1][1] = "244466666"
+arr[2][0] = "1gelklytorias"
+arr[2][1] = "qwerty"
+arr[3][0] = "sviganast"
+arr[3][1] = "1357913579"
+arr[4][0] = "dimitrog"
+arr[4][1] = "asdfghjkl"
+
+
 
 ###frames
 frame_temp=Frame()#Frame to get as temp to successfull change between frames
 all_Frame=Frame(main_window, bg="white")
 menu_Frame=Frame(all_Frame, bg="gray26")
 intro_Frame = Frame(all_Frame, bg="floral white")
-application_Frame=Frame(all_Frame, bg="floral white")
-pending_applic_Frame=Frame(all_Frame, bg="floral white") #aitiseis
-applic_verify_Frame=Frame(all_Frame, bg="floral white")#pendingaitiseis
-department_application_Frame=Frame(all_Frame, bg="floral white")#dhlwsh thesewn
-statement_Frame4=Frame(all_Frame, bg="floral white")#dhmiourgia
-page_Frame=Frame(all_Frame, bg="floral white")#panellhnies
-page_edit_Frame=Frame(all_Frame, bg="floral white")
-capacity_Frame=Frame(all_Frame, bg="floral white")#programma
-capacity_submit_Frame=Frame(all_Frame, bg="floral white")#eksetastiko kentro
-graderslist_frame=Frame(all_Frame, bg="floral white") #vathmologites
-acceptdeny_frame=Frame(all_Frame, bg="floral white") #apodoxi aporripsi 
-processing_frame=Frame(all_Frame, bg="floral white") #epeksergasia
-problems_frame=Frame(all_Frame, bg="floral white") #provlimata
 exit_frame=Frame(all_Frame, bg="floral white") #eksodos
 
 
@@ -114,8 +121,27 @@ def main():
     login_passw2 = Text(login_passw, bg="WHITE", height=1, width=40, fg="black", borderwidth=1, highlightthickness=2,font=("Times New Roman (Times)", 16))
 
     label_buttons = Label(label_top3, bg="floral white")
-    btn_acccept = Button(label_buttons, text="Σύνδεση", bg="green3",font=("Times New Roman (Times)", 14, "bold"),height=1 ,width=12)
+    btn_acccept = Button(label_buttons, text="Σύνδεση", command=lambda: datacheck(), bg="green3",font=("Times New Roman (Times)", 14, "bold"),height=1 ,width=12)
     btn_exit = Button(label_buttons, text="Έξοδος", command=lambda: ExitApp(), bg="gray",font=("Calibri", 14, "bold"),height=1 ,width=12)
+
+
+    def datacheck():
+        usr_name = login_usr2.get('1.0', 'end-1c')
+        usr_pass = login_passw2.get('1.0', 'end-1c')
+
+        if arr[0][0]==usr_name and arr[0][1]==usr_pass:
+            initUI.main()
+        elif arr[1][0]==usr_name and arr[1][1]==usr_pass:
+            department.main()
+        elif arr[2][0]==usr_name and arr[2][1]==usr_pass:
+            school.main()
+        elif arr[3][0]==usr_name and arr[3][1]==usr_pass:
+            Grader.main()
+        elif arr[4][0]==usr_name and arr[4][1]==usr_pass:
+            board.main()    
+        else:
+            messagebox.showinfo('Ανεπιτυχής Σύνδεση', 'Παρακαλώ πληκτρολογείστε τα σωστά στοιχεία.', icon='error')
+
 
 
     login_all.pack(side = TOP, fill=BOTH, expand=1)
